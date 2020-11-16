6878c53ea87cdb40067e40e57f7705513239f4d3
Doiii14 <60578111+Doiii14@users.noreply.github.com>
2020-11-16 15:31:51 +0100

    Add files via upload

---
+++ botMain.js
@@ -0,0 +1,36 @@
+
+var TelegramBot = require('node-telegram-bot-api');
+var token = '1477150367:AAFcLvw2i2ruqifRt_KUZjbTBc43c7wSzGQ';
+var bot = new TelegramBot(token,{polling:true});
+var request = require('request');
+
+bot.on('message', (msg) => {
+    bot.sendMessage(msg.chat.id, "<b>Type /search filmName to get info about your favorite film </b>",{parse_mode:"HTML"});
+});
+
+bot.onText(/\/search (.+)/,function(msg,match){
+    let idchat = msg.chat.id;
+    let movie = match[1]; //LETTURA NOME
+    if (movie.match(/[a-z]/i)){
+        bot.on("polling_error", (err) => console.log(err));
+        let url = 'http://www.omdbapi.com/?apikey=1abe5d65&t=$film';
+        let finalstring = url.replace('film', movie);
+        request(finalstring,function(error,response,body){
+
+        let res = JSON.parse(body);
+        console.log(finalstring);
+        if(!error && response.statusCode == 200 && res.Response == "True"){
+            bot.sendMessage(idchat, '<em>Looking for </em>' + movie +'...',{parse_mode:"HTML"}).then(function(msg){
+            bot.sendMessage(idchat,'<b>🚨 Result:</b> \n\n <b>✅ Title: </b>' +  res.Title 
+            +"\n <b>✅ Release: </b>" + res.Released + "\n <b>✅ Genre: </b> " + res.Genre + "\n <b>✏ Writer: </b> "+ res.Writer 
+            +"<b>\n 🌟 Ratings</b> " + res.imdbRating, {parse_mode:"HTML"});
+            bot.sendPhoto(idchat,res.Poster);
+            })
+        }else if(!error && res.Response == "False"){
+            bot.sendMessage(idchat,"<b>I didn't found nothing... 😭</b>",{parse_mode:"HTML"});
+        }
+    });
+    }else{
+        bot.sendMessage(idchat,"<b> Buddy, you need to use alphabetical character! 🚨</b>",{parse_mode:"HTML"});
+    }            
+})
